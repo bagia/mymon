ab7f91a8da0e309dcf2a35587505384fe7a672d8
bagia <neobaub@gmail.com>
2013-12-26 23:46:54 +0100

    Bug in CLI Execution of AsyncTask

--- install.sql
+++ install.sql
@@ -3,14 +3,14 @@
 --
 
 DROP TABLE IF EXISTS `history`;
-CREATE TABLE IF NOT EXISTS `history` (
+CREATE TABLE `history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `watchdog` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_agent` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `watchdog_idx` (`watchdog`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -19,22 +19,14 @@ CREATE TABLE IF NOT EXISTS `history` (
 --
 
 DROP TABLE IF EXISTS `watchdog`;
-CREATE TABLE IF NOT EXISTS `watchdog` (
+CREATE TABLE `watchdog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `fb_article` varchar(255) DEFAULT NULL,
+  `fb_app` varchar(255) DEFAULT NULL,
   `notify_user` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `history`
---
-ALTER TABLE `history`
-  ADD CONSTRAINT `watchdog_ct` FOREIGN KEY (`watchdog`) REFERENCES `watchdog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
--- www/vendors/AsyncTask.PHP/src/CliExecution.php
+++ www/vendors/AsyncTask.PHP/src/CliExecution.php
@@ -14,12 +14,11 @@ class CliExecution implements Execution {
 
         if ($this->isWindows()) {
             $startbg = realpath(__DIR__ . '/startbg/bin/startbg.exe');
-            $command_line = "{$startbg} C:\\php\\php.exe {$file_name} at_exec {$task->getIdentifier()}{$dependencies} >NUL 2>NUL";
-            //$command_line = "{$startbg} php C:/temp/sleep.php";
+            $command_line = "{$startbg} php {$file_name} at_exec {$task->getIdentifier()}{$dependencies} >NUL 2>NUL";
             system($command_line);
         } else {
             $command_line = "php {$file_name} at_exec {$task->getIdentifier()}{$dependencies} > /dev/null 2>&1 &";
-            exec($command_line);
+            system($command_line);
         }
     }
 
