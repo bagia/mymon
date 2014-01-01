315d8c62ea9772c1f415bfc5ec61bde5967f188f
bagia <neobaub@gmail.com>
2014-01-01 18:41:31 +0100

    Refactoring task progress handling SQL

--- install.sql
+++ install.sql
@@ -2,8 +2,7 @@
 -- Table structure for table `history`
 --
 
-DROP TABLE IF EXISTS `history`;
-CREATE TABLE `history` (
+CREATE TABLE IF NOT EXISTS `history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `watchdog` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -15,18 +14,32 @@ CREATE TABLE `history` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `watchdog`
 --
 
-DROP TABLE IF EXISTS `watchdog`;
-CREATE TABLE `watchdog` (
+CREATE TABLE IF NOT EXISTS `watchdog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` varchar(255) NOT NULL,
+  `friend_id` varchar(255) DEFAULT NULL,
   `image` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `fb_article` varchar(255) DEFAULT NULL,
   `fb_app` varchar(255) DEFAULT NULL,
   `notify_user` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
