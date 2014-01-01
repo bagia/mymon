5ee6f5faa24cf383a13b3f53e4e7835b9f1028e7
bagia <neobaub@gmail.com>
2014-01-01 23:42:42 +0100

    Updating privacy policy

--- www/ui/partials/privacy.php
+++ www/ui/partials/privacy.php
@@ -7,11 +7,13 @@
     <p>After authenticating to Facebook you will see your name and profile picture
         displayed on the site. We do not store them.</p>
     <h4>Information we store</h4>
-    <p>All the watchdogs you create are associated with your Facebook <i>third party
-       id</i> which is an identifier that does not let us identify you namely.</p>
-    <p>If you decide to activate <i>notifications</i> on any of your watchdogs, then
-        we will have to store your Facebook user id - that permits identification
-        by name.</p>
-    <p>No other information is stored from your Facebook account.</p>
+    <p>All the watchdogs you create are associated with your Facebook id which is a unique
+        identifier that allows to namely identify you, and sometimes the article id that
+        allows to identify the specific publication that was made.</p>
+    <p>If you decide to create a <i>Power Watchdog</i>, then  we will have to store all of
+        your Facebook friends names. We won't make them public nor share them with anyone.</p>
+    <p>Other information stored is the date of creation of a watchdog, the history of the hits
+        of a watchdog, and whether or not a power watchdog is being deployed or deleted on your
+        profile.</p>
 </div>
 
